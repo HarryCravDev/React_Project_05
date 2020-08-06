abc2ab40de972adef50b3e40eaa7b1ce8e65d101
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-06 23:41:44 +0100

    Update to App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Die from "./Components/Die/Die";
+import RollDice from "./Components/Roll_Dice/RollDice";
 
 function App() {
   return (
     <div className="App">
-      <Die face="one" />
-      <Die face="three" />
-      <Die face="six" />
+      <RollDice />
     </div>
   );
 }
