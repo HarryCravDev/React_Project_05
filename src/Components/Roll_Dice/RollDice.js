64d3844e16ef868e11f4808596c5dd08e02453fc
Harry Craven <HarryCraven.Dev@gmail.com>
2020-08-07 16:07:30 +0100

    Added dynamic text and disabled tag to button.

--- src/Components/Roll_Dice/RollDice.js
+++ src/Components/Roll_Dice/RollDice.js
@@ -10,6 +10,7 @@ class RollDice extends React.Component {
   state = {
     die1: "one",
     die2: "one",
+    rolling: false,
   };
 
   roll = () => {
@@ -25,7 +26,12 @@ class RollDice extends React.Component {
     this.setState({
       die1: newDie1,
       die2: newDie2,
+      rolling: true,
     });
+
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 500);
   };
 
   render() {
@@ -35,7 +41,9 @@ class RollDice extends React.Component {
           <Die face={this.state.die1} />
           <Die face={this.state.die2} />
         </div>
-        <button onClick={this.roll}>Roll!</button>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+        </button>
       </div>
     );
   }
